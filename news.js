7339bb9bc8fd8eaee5446576c15160d046466ca1
sigmaweb1811 <38896560+sigmaweb1811@users.noreply.github.com>
2018-07-16 00:15:21 +0200

    Update news.js

--- news.js
+++ news.js
@@ -8,24 +8,6 @@
  *     close_button
  */
 
-function recent_post_content(){
-    var s = "<div id='recent_post' title='Breaking'>";
-    if(info_text){
-    s += "<div class='wrapper'>";
-    s += "<div class='newstitle'>";
-    s += "Breaking";
-    s += "</div>";
-    }
-    s += "  <marquee style='float:left; margin-left:10px; line-height: 24px; width:82%' scrollAmount='"+scrolling_speed+"'>";
-    for(var i=0; i<latest_post; i++){
-        var recent_post_entries = entries[i];
-        s += "<a href='"+recent_post_entries.href+"' ";
-        s += "onmouseover='this.parentNode.stop()' onmouseout='this.parentNode.start()'";
-        s += ">" + recent_post_entries.title + "</a>";
-        if(i != latest_post-1){s += " | ";}
-	function myFunction() {
-    document.getElementById("myP").style.textAlign = "right";
-}
 
     }
     s += "</marquee>";
